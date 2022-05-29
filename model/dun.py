454708cb6454e885bf0992691e82c134433ef2a3
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-05-29 21:53:37 +0200

    Update dun validation

--- model/dun.py
+++ model/dun.py
@@ -1,7 +1,13 @@
 from typing import List, Union
+from pydantic import validator
 from .api_base_model import ApiBaseModel
 
 class DUN(ApiBaseModel):
   identifier: str
-  
 
+  @validator('identifier')
+  def identifier_must_be_valid(cls, identifier):
+    if len(identifier) == 9 and identifier.isdigit():
+      return identifier
+    else:
+      raise ValueError('must ba a 9 digit string')
