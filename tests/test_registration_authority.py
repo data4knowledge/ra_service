d274e3c28dc909aa1dcca25e5a92dec5dc697298
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-05-30 21:36:30 +0200

    Further tests

--- tests/test_registration_authority.py
+++ tests/test_registration_authority.py
@@ -1,7 +1,7 @@
 import pytest
 from model.registration_authority import RegistrationAuthority
 
-def test_registration_authority():
+def test_registration_authority_ror():
     params = { 'name': 'Jack', 'namespace': '12345', 'ror': {'identifier': 'xxx'} }
     item = RegistrationAuthority(**params)
     assert item.name == "Jack"
@@ -10,4 +10,45 @@ def test_registration_authority():
     assert item.grid == None
     assert item.company == None
     assert item.dun == None
+
+def test_registration_authorit_dun():
+    params = { 'name': 'Jack', 'namespace': '12345', 'dun': {'identifier': '123456789'} }
+    item = RegistrationAuthority(**params)
+    assert item.name == "Jack"
+    assert item.namespace == '12345'
+    assert item.dun == {'identifier': '123456789'} 
+    assert item.ror == None
+    assert item.company == None
+    assert item.grid == None
+
+def test_registration_authorit_grid():
+    params = { 'name': 'Jack', 'namespace': '12345', 'grid': {'identifier': 'xxx'} }
+    item = RegistrationAuthority(**params)
+    assert item.name == "Jack"
+    assert item.namespace == '12345'
+    assert item.grid == {'identifier': 'xxx'} 
+    assert item.ror == None
+    assert item.company == None
+    assert item.dun == None
+
+def test_registration_authorit_company():
+    params = { 'name': 'Jack', 'namespace': '12345', 'company': {'identifier': 'xxx', 'country_code': 'GBR'} }
+    item = RegistrationAuthority(**params)
+    assert item.name == "Jack"
+    assert item.namespace == '12345'
+    assert item.grid == None
+    assert item.ror == None
+    assert item.company == {'identifier': 'xxx', 'country_code': 'GBR'} 
+    assert item.dun == None
+
+def test_registration_authorit_company_error():
+    params = { 'name': 'Jack', 'namespace': '12345', 'company': {'identifier': 'xxx', 'country_code': 'GB'} }
+    with pytest.raises(ValueError):
+      item = RegistrationAuthority(**params)
+
+def test_registration_authorit_dun_error():
+    params = { 'name': 'Jack', 'namespace': '12345', 'dun': {'identifier': '12345678'} }
+    with pytest.raises(ValueError):
+      item = RegistrationAuthority(**params)
+
 
